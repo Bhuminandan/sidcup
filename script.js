c532bf981f772414f6b4d9dc18c6656520609510
Bhumi <bhumione9545@gmail.com>
2023-07-02 19:48:30 +0530

    Added remaining sections like slider and info dive looking forward to add responsive changes soon

--- script.js
+++ script.js
@@ -119,3 +119,34 @@ gsap.from("#page4 h1", {
         scrub: 2
     }
 })
+
+
+var index = 0;
+var slides = document.querySelectorAll(".slides");
+var dot = document.querySelectorAll(".dot");
+
+function changeSlide(){
+
+  if(index<0){
+    index = slides.length-1;
+  }
+  
+  if(index>slides.length-1){
+    index = 0;
+  }
+  
+  for(let i=0;i<slides.length;i++){
+    slides[i].style.display = "none";
+    dot[i].classList.remove("active");
+  }
+  
+  slides[index].style.display= "block";
+  dot[index].classList.add("active");
+  
+  index++;
+  
+  setTimeout(changeSlide,2000);
+  
+}
+
+changeSlide();
